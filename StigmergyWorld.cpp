41d91230de4edb28c7e53ca111bb3a3a969fd99f
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2018-03-01 13:35:02 -0500

    initial commit

---
+++ StigmergyWorld.cpp
@@ -0,0 +1,35 @@
+//  MABE is a product of The Hintze Lab @ MSU
+//     for general research information:
+//         hintzelab.msu.edu
+//     for MABE documentation:
+//         github.com/Hintzelab/MABE/wiki
+//
+//  Copyright (c) 2015 Michigan State University. All rights reserved.
+//     to view the full license, visit:
+//         github.com/Hintzelab/MABE/wiki/License
+
+#include "StigmergyWorld.h"
+//User Parameters
+shared_ptr<ParameterLink<int>> StigmergyWorld::evaluationsPerGenerationPL = Parameters::register_parameter("WORLD_STIGMERGY-evaluationsPerGeneration", 1, "Number of times to test each Genome per generation (useful with non-deterministic brains)");
+//MABE Parameters
+shared_ptr<ParameterLink<string>> StigmergyWorld::groupNamePL = Parameters::register_parameter("WORLD_STIGMERGY_NAMES-groupNameSpace", (string)"root::", "namespace of group to be evaluated");
+shared_ptr<ParameterLink<string>> StigmergyWorld::brainNamePL = Parameters::register_parameter("WORLD_STIGMERGY_NAMES-brainNameSpace", (string)"root::", "namespace for parameters used to define brain");
+
+StigmergyWorld::StigmergyWorld(shared_ptr<ParametersTable> _PT):AbstractWorld(_PT) {
+	// columns to be added to ave file
+	popFileColumns.clear();
+	popFileColumns.push_back("score");
+}
+
+void StigmergyWorld::evaluateSolo(shared_ptr<Organism> org, int analyze, int visualize, int debug) {
+	auto brain = org->brains[brainNamePL->get(PT)];
+	for (int eval = 0; eval < evaluationsPerGenerationPL->get(PT); eval++) {
+		brain->resetBrain();
+		brain->setInput(0, 1);
+		brain->update();
+		int i = 1;
+		int dummy = Bit(brain->readOutput(i));
+		double score = 1.0;
+		org->dataMap.append("score", score);
+	}	
+}
---
+++ StigmergyWorld.h
@@ -0,0 +1,48 @@
+//  MABE is a product of The Hintze Lab @ MSU
+//     for general research information:
+//         hintzelab.msu.edu
+//     for MABE documentation:
+//         github.com/Hintzelab/MABE/wiki
+//
+//  Copyright (c) 2015 Michigan State University. All rights reserved.
+//     to view the full license, visit:
+//         github.com/Hintzelab/MABE/wiki/License
+
+#pragma once
+
+#include "../AbstractWorld.h"
+
+#include <stdlib.h>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+class StigmergyWorld : public AbstractWorld {
+private:
+	int inputSize = 1;
+	int outputSize = 1;
+public:
+	//User Parameters
+	static shared_ptr<ParameterLink<int>> evaluationsPerGenerationPL;
+	//MABE Parameters
+	static shared_ptr<ParameterLink<string>> groupNamePL;
+	static shared_ptr<ParameterLink<string>> brainNamePL;
+
+	StigmergyWorld(shared_ptr<ParametersTable> _PT = nullptr);
+	virtual ~StigmergyWorld() = default;
+
+	virtual void evaluateSolo(shared_ptr<Organism> org, int analyze, int visualize, int debug);
+
+	virtual void evaluate(map<string, shared_ptr<Group>>& groups, int analyze, int visualize, int debug) {
+		int popSize = groups[groupNamePL->get(PT)]->population.size();
+		for (int i = 0; i < popSize; i++) {
+			evaluateSolo(groups[groupNamePL->get(PT)]->population[i], analyze, visualize, debug);
+		}
+	}
+
+	virtual unordered_map<string, unordered_set<string>> requiredGroups() override {
+		return { { groupNamePL->get(PT),{ "B:" + brainNamePL->get(PT) + "," + to_string(inputSize)+ "," + to_string(outputSize) } } };
+	}
+};
+
